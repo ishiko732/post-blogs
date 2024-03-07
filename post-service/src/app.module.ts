11ad50c79ffee3405da360a1581cb970ec7d7581
ishiko732 <ishiko732@gmail.com>
2024-03-07 14:01:33 +0800

    init posts

--- post-service/src/app.module.ts
+++ post-service/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
+import { PostsModule } from '@posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
